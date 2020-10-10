c813ac3e7c3c7d81e5590706921595048b9d1f88
L磊 刘 <550856772@qq.com>
2020-10-10 17:16:15 +0800

    添加项目文件。

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        //display特性指定要显示的字段名称的内容
+        //datatype属性指定数据的类型
+        [Display(Name ="Release Date")]
+        [DataType(DataType.Date)] 
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        [Column(TypeName ="decimal(18,2)")]
+        public decimal Price { get; set; }
+    }
+}
